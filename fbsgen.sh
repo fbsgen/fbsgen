022289947057e2f1972886709b41e07748502696
dyu <david.yu.ftw@gmail.com>
2014-09-10 17:03:38 +0800

    convenience script

---
+++ fbsgen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CURRENT_DIR=$PWD
+
+[ ! -e $CURRENT_DIR/codegen.properties ] && echo "codegen.properties not found on current dir." && exit 0
+
+SCRIPT_DIR=$CURRENT_DIR
+
+if [ ! -e $SCRIPT_DIR/fbsgen/base.stg ]; then
+    
+    SCRIPT=$(readlink -f "$0")
+    # Absolute path this script is in
+    SCRIPT_DIR=$(dirname "$SCRIPT")
+fi
+
+JAR_FILE=$SCRIPT_DIR/fbsgen-compiler/target/fbsgen.jar
+[ -e $SCRIPT_DIR/fbsgen.jar ] && JAR_FILE=$SCRIPT_DIR/fbsgen.jar
+
+PROTO_PATH=.
+[ -d $CURRENT_DIR/proto ] && PROTO_PATH=proto
+
+# -Dproto_search_strategy=2 means search from classpath if the file is not found.
+
+java -Dproto_path=$PROTO_PATH \
+    -Dproto_search_strategy=2 \
+    -Dfbsgen.print_stack_trace=false \
+    -Dfbsgen.sequential_field_numbers=true \
+    -Dfbsgen.resolve_enum_value_ref=true \
+    -jar $JAR_FILE codegen.properties $@
