876be3a5ba420da459b214cd657a2d0d9f64fdb0
dyu <david.yu.ftw@gmail.com>
2014-09-11 16:58:23 +0800

    do

--- fbsgen.sh
+++ fbsgen.sh
@@ -16,16 +16,20 @@ fi
 JAR_FILE=$SCRIPT_DIR/fbsgen-compiler/target/fbsgen.jar
 [ -e $SCRIPT_DIR/lib/fbsgen.jar ] && JAR_FILE=$SCRIPT_DIR/lib/fbsgen.jar
 
-TEMPLATE_PATH=.
-[ -d $CURRENT_DIR/templates ] && TEMPLATE_PATH=templates
+if [ -z $TEMPLATE_PATH ]; then
+    TEMPLATE_PATH=.,$SCRIPT_DIR/templates
+    [ -d $CURRENT_DIR/templates ] && TEMPLATE_PATH=templates,$SCRIPT_DIR/templates
+fi
 
-PROTO_PATH=.
-[ -d $CURRENT_DIR/proto ] && PROTO_PATH=proto
+if [ -z $PROTO_PATH ]; then
+    PROTO_PATH=.
+    [ -d $CURRENT_DIR/proto ] && PROTO_PATH=proto
+fi
 
 # -Dproto_search_strategy=2 means search from classpath if the file is not found.
 # The search strategy for templates is the same as above (except it cannot be changed).
 
-java -Dtemplate_path=$TEMPLATE_PATH,$SCRIPT_DIR/templates \
+java -Dtemplate_path=$TEMPLATE_PATH \
     -Dproto_path=$PROTO_PATH \
     -Dproto_search_strategy=2 \
     -Dfbsgen.print_stack_trace=false \
