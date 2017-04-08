2d8ac2fc84a7c72c8a520c4fe189b30349965d31
dyu <david.yu.ftw@gmail.com>
2017-04-08 20:51:26 +0800

    generateBackupPoms=false

--- release.sh
+++ release.sh
@@ -11,9 +11,9 @@ PR_VERSION=$2
 
 echo "Releasing $VERSION - are you sure? (y/n):" && read CONFIRM && [ "$CONFIRM" != "y" ] && exit 0
 
-mvn versions:set -DnewVersion=$VERSION && \
-git clean -f && git add -u && git commit -m "$VERSION" && \
+mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false && \
+git add -u . && git commit -m "$VERSION" && \
 mvn -Prelease deploy && mvn scm:tag && \
-mvn versions:set -DnewVersion=$PR_VERSION && \
-git clean -f && git add -u && git commit -m "$PR_VERSION" && \
+mvn versions:set -DnewVersion=$PR_VERSION -DgenerateBackupPoms=false && \
+git add -u . && git commit -m "$PR_VERSION" && \
 git push origin master
